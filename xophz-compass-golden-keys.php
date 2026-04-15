7fe10e818c584aa01e81a1f522f4734dfd0d54c3
Xopher <xopher@vi.co>
2026-04-14 21:30:18 -0700

    chore: sync plugins

--- xophz-compass-golden-keys.php
+++ xophz-compass-golden-keys.php
@@ -19,7 +19,7 @@
  * Description:       Discover what golden keywords help unlock/open otherwise missed opportunities.
  * Version:           26.4.14.1311
  * Author:            Hall of the Gods, Inc.
- * Author URI:        http://www.midnightnerd.com/
+ * Author URI:        http://www.hallofthegods.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       xophz-compass-golden-keys
